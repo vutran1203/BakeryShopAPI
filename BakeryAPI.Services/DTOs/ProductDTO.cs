94c33d6df1ead563dbb2c1b1322b42565fda0724
vutran1203 <vt41247@gmail.com>
2025-12-05 13:56:45 +0700

    Auto update code

--- BakeryAPI.Services/DTOs/ProductDTO.cs
+++ BakeryAPI.Services/DTOs/ProductDTO.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; } // <--- BẮT BUỘC PHẢI CÓ DÒNG NÀY
         public string Name { get; set; }
         public decimal Price { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public string ImageUrl { get; set; }
         public string CategoryName { get; set; }
     }
